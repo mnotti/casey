d509ceb5503919756c85191a25050a67df202bcd
mnotti <markusnotti@gmail.com>
2023-06-14 18:25:37 -0700

    Yfinance takes parms for grabbing stock price data

--- yfinance.py
+++ yfinance.py
@@ -3,26 +3,31 @@ from yahoo_fin import stock_info as si
 import pandas as pd
 import json
 import os
+import sys
 
 with open('config.json') as f:
     config = json.load(f)
 price_folder_path = config['price_path']
 
-# Define the stock symbol and the date range
-symbol = "TSLA"
-end_date = datetime.now().date()
-start_date = end_date - timedelta(days=30)
+def get_price_data(symbol, start_date_str, end_date_str):
+	start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+	end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
-# Retrieve the stock data using yahoo_fin
-data = si.get_data(symbol, start_date, end_date)
+	data = si.get_data(symbol, start_date, end_date)
+	stock_data = data[["open", "close", "low", "high", "volume"]]
 
-# Extract the desired columns
-stock_data = data[["open", "close", "low", "high", "volume"]]
+	if not os.path.exists(price_folder_path):
+		os.makedirs(price_folder_path)
+	csv_filename = f"price-{symbol}.csv"
+	csv_path = os.path.join(price_folder_path, csv_filename)
+	stock_data.to_csv(csv_path, index=True, header=True, index_label="date")
 
-if not os.path.exists(price_folder_path):
-	os.makedirs(price_folder_path)
-csv_filename = f"price-{symbol}.csv"
-csv_path = os.path.join(price_folder_path, csv_filename)
+def main():
+	symbol = sys.argv[1]
+	start_date = sys.argv[2]
+	end_date = sys.argv[3]
+	print(f"Running get_price_data w/ inputs ({symbol}, {start_date}, {end_date})...")
+	get_price_data(symbol, start_date, end_date)
 
-# Write the stock_data DataFrame to a CSV file
-stock_data.to_csv(csv_path, index=True, header=True, index_label="date")
+if __name__ == "__main__":
+	main()
